b0ccfddeb653364a7ea369cb6229dd91f4e28404
Chris Bruce <22107777+cb2292@users.noreply.github.com>
2018-09-02 18:49:04 -0400

    logo update and formatting tweaks

--- static/script.js
+++ static/script.js
@@ -3,11 +3,11 @@
 $(document).ready(function() {
 	$(".ishidden").hide();
 	$(".faq_link").click(function() {
-		if ($(this).parent().next().hasClass("ishidden") === true)	{
-			$(this).parent().next().show();
-		}
-		else	{
-			return null;
-		}
+        if ($(this).parent().next().hasClass("ishidden") === true)    {
+            $(this).parent().next().show();
+        }
+        else    {
+            return null;
+        }
 	});
 });
