d52297cdf19eaf5384dad4e9704abddcac1b8a9b
Chris Bruce <22107777+cb2292@users.noreply.github.com>
2018-09-03 14:28:58 -0400

    responsive format tweak and jQuery cleanup

--- static/script.js
+++ static/script.js
@@ -3,11 +3,6 @@
 $(document).ready(function() {
 	$(".ishidden").hide();
 	$(".faq_link").click(function() {
-        if ($(this).parent().next().hasClass("ishidden") === true)    {
-            $(this).parent().next().show();
-        }
-        else    {
-            return null;
-        }
+			$(this).parent().next().toggle();
 	});
 });
